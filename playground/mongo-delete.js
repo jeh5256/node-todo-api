013cb9b5d68321a4fd1f75e8cd709ba107030b33
Justin Hughes <jeh5256@gmail.com>
2017-11-25 21:21:24 -0600

    updates

--- playground/mongo-delete.js
+++ playground/mongo-delete.js
@@ -7,8 +7,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
+    //deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) =>{
+    //     console.log(result);
+    // });
 
-    db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) =>{
-        console.log(result);
-    });
+    //deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) =>{
+    //     console.log(result);
+    // });
+
+    //findOneAndDelete
 });
