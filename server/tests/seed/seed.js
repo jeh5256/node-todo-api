372b51468b830d8e93805a24be1fa085b6c82439
Justin <jeh5256@gmail.com>
2017-12-31 18:56:24 -0600

    add function to remove old records from database and repopulate with test data

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,52 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneID = new ObjectID();
+const userTwoID = new ObjectID();
+
+const users = [{
+    _id: userOneID,
+    email: 'jeh5256123@gmail.com',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneID, access: 'auth'}, 'abc123').toString()
+    }]
+}, {
+    _id: userTwoID,
+    email: 'gen@example.com',
+    password: 'xwuserTwoPass'
+}];
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+}, {
+    _id: new ObjectID(),
+    text: 'Second test todo'
+}, {
+    _id: new ObjectID(),
+    text: 'Third test todo',
+    completed: true,
+    completedAt: 333
+}];
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done()).catch((e) => {console.log(e)});
+};
+
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne, userTwo]).then(() => done());
+    });
+}
+
+module.exports = {todos, populateTodos, users, populateUsers};
